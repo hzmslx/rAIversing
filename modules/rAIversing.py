440cdf44e62b130e43df363142dffbb2ec32123f
MrMatch246 <s8daerce@stud.uni-saarland.de>
2023-03-09 19:08:44 +0100

    updated modules for usage with new features

--- modules/rAIversing.py
+++ modules/rAIversing.py
@@ -1,21 +1,30 @@
-from rAIversing.rAIverseEngine import rAIverseEngine
+from rAIversing.Engine import rAIverseEngine
 from rAIversing.AI_modules.openAI_core import chatGPT
-
+from rAIversing.evaluator import eval_p2im_firmwares
 
 
 
 def testbench():
-    string="uint getLowest16Bits(void) {\\n  return _DAT_00338138 & 0xffff;\\n}"
+    #string="int function_1_00084f54() {\n\\n    int result;\n\\n    if (*PTR_PTR_DAT_000850f0 != 0 && *PTR_DAT_000850ec != '\\\\0') {\n\\n      result = (*pointer_to_function_1_00084f54)();\n\\n      return result;\n\\n    }\n\\n    return 0;\n\\n  }"
+    string="\nvoid FUN_00098918(undefined4 *param_1)\n\n{\n  int iVar1;\n  \n  iVar1 = FUN_000987da(*param_1);\n  if (1 < *(byte *)(iVar1 + 8)) {\n    return;\n  }\n  *(undefined *)(iVar1 + 8) = 2;\n  FUN_000113e4(0x20000000);\n  return;\n}\n\n"
     print(string)
-    print(string.replace("\\n","\n"))
+    out = string.replace("\\n","\n")
+    print(out.replace('\\\\','\\'))
 
+def run20819():
+    raie=rAIverseEngine(chatGPT.v2_api_key(),binary_path="20819_firmware")
+    raie.load_functions()
+    #raie.run_recursive_rev()
+    raie.cleanup()
+    raie.export_processed(all_functions=True)
+
+def evaluation():
+    eval_p2im_firmwares()
 
 def main():
-    raie=rAIverseEngine(chatGPT.ChatGPTModule(),binary_path="20819_firmware")
-    raie.load_functions()
-    raie.run_recursive_rev()
-    #raie.export_processed()
-
+    #testbench()
+    #run20819()
+    evaluation()
 
 
 
--- modules/rAIversing/utils/__init__.py
+++ modules/rAIversing/utils/__init__.py
@@ -1,5 +1,7 @@
 import os, sys
 from rAIversing.pathing import *
+import tiktoken
+
 
 def check_and_fix_bin_path(binary_path):
     if os.path.abspath(binary_path) == binary_path:
@@ -9,3 +11,28 @@ def check_and_fix_bin_path(binary_path):
             return os.path.join(BINARIES_ROOT, binary_path)
         else:
             raise FileNotFoundError(f"Binary {binary_path} not found in {BINARIES_ROOT}")
+
+
+def undo_bad_renaming(renaming_dict, code):
+    for old, new in renaming_dict.items():
+        if "PTR" in old or "DAT" in old:
+            code = code.replace(new, old)
+        if "FUN_" in old and ("reverse" in new and "engineer" in new.lower()):
+            code = code.replace(new, old)
+    return code
+
+
+def extract_function_name(code):
+    if "WARNING: Removing unreachable block (ram," in code:
+        code = code.split("\n\n")[1].split("(")[0].split("\n")[-1].split(" ")[-1]
+        return code
+
+
+    return code.split("(")[0].split(" ")[-1]
+def generate_function_name(code,name):
+    return f"{extract_function_name(code)}_{name.replace('FUN_', '')}"
+
+
+def calc_used_tokens(function):
+    enc = tiktoken.encoding_for_model("text-davinci-002")
+    return len(enc.encode(function))
