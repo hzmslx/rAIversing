cd59b2db13faf2ebab30767634fa825552f8cdae
Daniel Erceg <s8daerce@stud.uni-saarland.de>
2023-03-27 18:25:40 +0200

    feat: eval

--- modules/rAIversing/evaluator/__init__.py
+++ modules/rAIversing/evaluator/__init__.py
@@ -3,6 +3,7 @@ import os, sys
 
 from rAIversing.AI_modules.openAI_core import chatGPT
 from rAIversing.CExporter.CExporter import binary_to_c_code
+from rAIversing.CExporter.CImporter import import_changes_to_ghidra_project
 from rAIversing.Engine import rAIverseEngine
 
 
@@ -11,9 +12,14 @@ def eval_p2im_firmwares(use_access_token=True):    #For now to prevent unwanted 
     for binary in usable_binaries:
         binary_path = f"{BINARIES_ROOT}/p2im/{binary}"
         print(f"Processing {binary}")
-        binary_to_c_code(binary_path, "ARM:LE:32:Cortex")
-        raie=rAIverseEngine(chatGPT.v2_api_key(), binary_path=binary_path)
-        raie.load_functions()
-        raie.run_recursive_rev()
+        if True:
+            binary_to_c_code(binary_path, "ARM:LE:32:Cortex")
+            raie=rAIverseEngine(chatGPT.access_token(), binary_path=binary_path)
+            raie.load_functions()
+            raie.run_recursive_rev()
+            raie.export_processed(all_functions=True)
+
+        import_changes_to_ghidra_project(binary_path, "ARM:LE:32:Cortex")
+
+
         #raie.cleanup()
-        raie.export_processed(all_functions=True)
