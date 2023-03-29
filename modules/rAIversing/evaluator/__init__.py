56020000c12241dae9e9bfe17e377dd0e2817e43
MrMatch246 <s8daerce@stud.uni-saarland.de>
2023-03-29 13:40:24 +0200

    refactor: changed parameters to take ai_module

--- modules/rAIversing/evaluator/__init__.py
+++ modules/rAIversing/evaluator/__init__.py
@@ -2,24 +2,23 @@ from rAIversing.pathing import *
 import os, sys
 
 from rAIversing.AI_modules.openAI_core import chatGPT
-from rAIversing.CExporter.CExporter import binary_to_c_code
-from rAIversing.CExporter.CImporter import import_changes_to_ghidra_project
+from rAIversing.Ghidra_Custom_API import binary_to_c_code, import_changes_to_ghidra_project
 from rAIversing.Engine import rAIverseEngine
 
 
-def eval_p2im_firmwares(use_access_token=True):    #For now to prevent unwanted costs but also doesn't work with the current model
+def eval_p2im_firmwares(ai_module):
     usable_binaries = ["Heat_Press"]               #os.listdir(f"{BINARIES_ROOT}/p2im")
     for binary in usable_binaries:
         binary_path = f"{BINARIES_ROOT}/p2im/{binary}"
         print(f"Processing {binary}")
         if True:
             binary_to_c_code(binary_path, "ARM:LE:32:Cortex")
-            raie=rAIverseEngine(chatGPT.access_token(), binary_path=binary_path)
+            raie=rAIverseEngine(ai_module, binary_path=binary_path)
             raie.load_functions()
             raie.run_recursive_rev()
             raie.export_processed(all_functions=True)
 
-        import_changes_to_ghidra_project(binary_path, "ARM:LE:32:Cortex")
+        import_changes_to_ghidra_project(binary_path)
 
 
         #raie.cleanup()
